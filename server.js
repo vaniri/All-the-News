3c0fe185ab30b180a31ac01f1779a3f0c4c8f25d
Vaniri <foxifennec@gmail.com>
2020-03-12 18:42:26 -0400

    added main layouts

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require("path");
 const mongoose = require('mongoose');
 const db = require('./models');
-const axios = require('axios');
 let Parser = require('rss-parser');
 let parser = new Parser();
 
@@ -13,11 +14,23 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/news', {
 const app = express();
 app.use(express.json());
 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//render main html page
+app.get("/", async (req, res) => {
+    res.render("index", {});
+});
+
+
+//app routes
 app.route('/news')
     .get(async (req, res) => {
         try {
-            let result = await db.News.find();
-            res.json({ result });
+            let result = await db.News.find().lean();
+            res.render("news", { news: result });
         } catch (err) {
             console.log("Error find news", err);
             res.json({ message: "FAIL", reason: err });
@@ -75,27 +88,28 @@ app.route('/comments/:id')
         }
     })
 
-    async function getNews () {
-        try {
-            let news = await parser.parseURL('http://feeds.foxnews.com/foxnews/latest');
-            news.items.forEach( async (item) => {
-                try {
-                    await db.News.create({ "headline": item.title, "author": item.creator, "pubDate": item.pubDate, "summary": item.contentSnippet, "url": item.link});
-                    console.log(item);
-                } catch (err) {
-                    if (err.code !== 11000) { //11000 is the duplicate key error code
-                        throw err;
-                    }
+async function getNews() {
+    try {
+        let news = await parser.parseURL('http://feeds.foxnews.com/foxnews/latest');
+        news.items.forEach(async (item) => {
+            try {
+                await db.News.create({ "headline": item.title, "author": item.creator, "pubDate": item.pubDate, "summary": item.contentSnippet, "url": item.link });
+                console.log(item);
+            } catch (err) {
+                if (err.code !== 11000) { //11000 is the duplicate key error code
+                    throw err;
                 }
-            });
-        } catch (err) {
-            console.log("Error find comments", err);
-            res.json({ message: "FAIL", reason: err });
-        }
+            }
+        });
+    } catch (err) {
+        console.log("Error find comments", err);
+        res.json({ message: "FAIL", reason: err });
     }
+}
 
-    getNews();
+getNews();
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('app listening at http://localhost:3000');
 });
+
